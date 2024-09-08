58b011b0485e0a67d9f5e8f72b848300f719921f
Dhananjay Sable <dhananjaysable370@gmail.com>
2024-09-08 16:32:49 +0530

    nextapp_02

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='h-auto md:h-[40rem] w-full rounded-md'>
+      
+    </div>
+  )
+}
+
+export default HeroSection
