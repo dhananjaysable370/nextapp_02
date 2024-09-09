09e5c3f53ed995b585eaf4c6f1ed8c813335678d
Dhananjay Sable <dhananjaysable370@gmail.com>
2024-09-09 17:00:55 +0530

    nextapp_02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolsTestimonials from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -7,7 +8,8 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
-      <WhyChooseUs/>
+      <WhyChooseUs />
+      <MusicSchoolsTestimonials/>
     </main>
   );
 }
